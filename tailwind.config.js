7d7368e668191862e7d24833ff6dc119582ad6ae
Pratyush0026 <113766134+Pratyush0026@users.noreply.github.com>
2025-10-14 22:33:23 +0530

    theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@ module.exports = {
   content: ["./**/*.html", "./assets/js/**/*.js"],
   theme: {
     extend: {
-      colors:{
-        primary:{
-          50:"#E6F3EB",
-          100:"#677471",
-          500:"#028835",
-          900:"#004C3F"
+      colors: {
+        primary: {
+          50: "#E6F3EB",
+          100: "#677471",
+          500: "#0163ea",
+          900: "#004C3F"
         },
-        secondary:"#FBF7ED",
-        primaryBorder:"#E3E3E3"
+        secondary: "#FBF7ED",
+        primaryBorder: "#E3E3E3"
       }
     },
   },
